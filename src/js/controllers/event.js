6958192b67597da2f7c9ae87fdea9d6c7f4e5b0f
michael grandison <michael.grandison@gmail.com>
2017-03-07 16:08:51 +0000

    fixed array comma split

--- src/js/controllers/event.js
+++ src/js/controllers/event.js
@@ -80,8 +80,9 @@ function NewEventFormController($auth, $state, Event) {
     newEventForm.newEvent.whodisliked = whoDisliked;
     newEventForm.newEvent.wholiked = whoLiked;
 
-    newEventForm.newEvent.positives = newEventForm.newEvent.positives.split(',');
-    newEventForm.newEvent.negatives = newEventForm.newEvent.negatives.split(',');
+
+    newEventForm.newEvent.positives.split(',');
+    newEventForm.newEvent.negatives.split(',');
     console.log('split positives', newEventForm.newEvent.positives);
     console.log('split negatives', newEventForm.newEvent.negatives);
 
